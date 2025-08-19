101510fc782131cd0c8577673173619647a3ef0b
lnkssr <gifzoner5@gamil.com>
2025-08-19 08:04:40 +0300

    fix code format

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
-#include "filePermission.hpp"
-#include "typeCheck.hpp"
 #include <filesystem>
 #include <iostream>
 
-int main(int argc, char *argv[]) {
+#include "filePermission.hpp"
+#include "typeCheck.hpp"
 
-  check_type_of_file(argc, argv);
-  return 0;
+int main(int argc, char *argv[]) {
+    check_type_of_file(argc, argv);
+    return 0;
 }
